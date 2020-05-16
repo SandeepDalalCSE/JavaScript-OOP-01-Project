f09d565542d6edbba7b262404aaf6539d2814026
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-16 07:36:52 +0530

    Adding products array

---
+++ assets/scripts/app.js
@@ -0,0 +1,16 @@
+// adding a list of products, say products array having individual product details in an object.
+const products = [
+  {
+    title: "A pillow",
+    imageUrl:
+      "https://ctl.s6img.com/society6/img/qq8CsYCPgLXfOUTRCkJzY6XrLLk/w_700/pillows/~artwork,fw_3500,fh_3500,iw_3500,ih_3500/s6-original-art-uploads/society6/uploads/misc/43abc139d2a448699550989add5a24cd/~~/kawaii-cute-panda-bear502620-pillows.jpg",
+    price: 150,
+    description: "a short pillow!"
+  },
+  {
+    title: "A carpet",
+    imageUrl: "https://images-na.ssl-images-amazon.com/images/I/81W6An71HSL._SX355_.jpg",
+    price: 500,
+    description: "a long caret!"
+  }
+];
