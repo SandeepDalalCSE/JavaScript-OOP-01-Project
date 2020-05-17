8815d53885413f9ffb5fecf58b2923f1ca09cbf3
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-17 07:25:21 +0530

    Creating ProductList Class

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -28,9 +28,10 @@ class Product {
 console.log(new Product("A carpet", "https://images-na.ssl-images-amazon.com/images/I/81W6An71HSL._SX355_.jpg", 500, "a long caret!"));
 // Consoling Product object
 
-//Converting this object literal concept into classes.
-const productList = {
-  products: [
+// Now Converting productList Object into ProductList Class
+class ProductList {
+  products = [
+    // Class field
     new Product(
       "A pillow",
       "https://ctl.s6img.com/society6/img/qq8CsYCPgLXfOUTRCkJzY6XrLLk/w_700/pillows/~artwork,fw_3500,fh_3500,iw_3500,ih_3500/s6-original-art-uploads/society6/uploads/misc/43abc139d2a448699550989add5a24cd/~~/kawaii-cute-panda-bear502620-pillows.jpg",
@@ -51,7 +52,7 @@ const productList = {
       price: 500,
       description: "a long caret!"
     }
-  ],
+  ];
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
@@ -75,6 +76,58 @@ const productList = {
     }
     renderHook.append(prodList);
   }
-};
-
+}
+console.log(new ProductList()); // ProductList Class or ProductList Object in general
+const productList = new ProductList();
 productList.render();
+
+//Converting this object literal concept into classes.
+// const productList = {
+//   products: [
+//     new Product(
+//       "A pillow",
+//       "https://ctl.s6img.com/society6/img/qq8CsYCPgLXfOUTRCkJzY6XrLLk/w_700/pillows/~artwork,fw_3500,fh_3500,iw_3500,ih_3500/s6-original-art-uploads/society6/uploads/misc/43abc139d2a448699550989add5a24cd/~~/kawaii-cute-panda-bear502620-pillows.jpg",
+//       150,
+//       "a short pillow!"
+//     ),
+//     new Product("A carpet", "https://images-na.ssl-images-amazon.com/images/I/81W6An71HSL._SX355_.jpg", 500, "a long caret!"),
+//     {
+//       title: "A pillow",
+//       imageUrl:
+//         "https://ctl.s6img.com/society6/img/qq8CsYCPgLXfOUTRCkJzY6XrLLk/w_700/pillows/~artwork,fw_3500,fh_3500,iw_3500,ih_3500/s6-original-art-uploads/society6/uploads/misc/43abc139d2a448699550989add5a24cd/~~/kawaii-cute-panda-bear502620-pillows.jpg",
+//       price: 150,
+//       description: "a short pillow!"
+//     },
+//     {
+//       title: "A carpet",
+//       imageUrl: "https://images-na.ssl-images-amazon.com/images/I/81W6An71HSL._SX355_.jpg",
+//       price: 500,
+//       description: "a long caret!"
+//     }
+//   ],
+//   render() {
+//     const renderHook = document.getElementById("app");
+//     const prodList = document.createElement("ul");
+//     prodList.className = "product-list";
+//     for (const prod of this.products) {
+//       //this => productList Object
+//       const prodEl = document.createElement("li");
+//       prodEl.className = "product-item";
+//       prodEl.innerHTML = `
+//       <div>
+//       <img src="${prod.imageUrl}" alt="${prod.title}">
+//       <div class="product-item__content">
+//         <h2>${prod.title}</h2>
+//         <h3>Rs ${prod.price}</h3>
+//         <p>${prod.description}</p>
+//         <button>Add to Cart</button>
+//       </div>
+//       </div>
+//       `;
+//       prodList.append(prodEl);
+//     }
+//     renderHook.append(prodList);
+//   }
+// };
+
+// productList.render();
